b1a09cbd92c5b32cbb315e8284635d7bdd31bacb
ind3ci5ive <39619831+ind3ci5ive@users.noreply.github.com>
2024-11-01 22:56:19 -0500

    Add fade animation for +1

--- javascript.js
+++ javascript.js
@@ -5,3 +5,15 @@ function hawktuah() {
 }
 
 document.getElementsByClassName("button")[0].addEventListener("click", hawktuah);
+
+
+// const canvas = document.querySelector("canvas");
+// const width = (canvas.width = window.innerWidth);
+// const height = (canvas.height = window.innerHeight);
+// const ctx = canvas.getContext("2d");
+
+// ctx.fillStyle = "rgb(255 0 0)";
+// ctx.fillRect(50, 50, 100, 150);
+
+// ctx.fillStyle = "rgb(0 255 0 / 75%)";
+// ctx.fillRect(75, 75, 100, 100);
