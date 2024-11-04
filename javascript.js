c4c3b201219ffc8c159315f29e081569d4178eac
ind3ci5ive <39619831+ind3ci5ive@users.noreply.github.com>
2024-11-04 17:03:07 -0600

    Attempt to add animation w/ button press; I hate googling code

--- javascript.js
+++ javascript.js
@@ -6,21 +6,13 @@ function hawktuah() {
 
 function plusOne() {
 
-    let x = document.getElementsByClassName("display-num")
+    const button = document.querySelector('.display-num');
+    const animation = [{animation: "fade 2s linear forwards"}];
+    // button.classList.toggle()
+    button.animate(animation);
+    console.log("function running");
 }
 
-elementName
-
 document.getElementsByClassName("button")[0].addEventListener("click", hawktuah);
+document.getElementsByClassName("button")[0].addEventListener("click", plusOne);
 
-
-// const canvas = document.querySelector("canvas");
-// const width = (canvas.width = window.innerWidth);
-// const height = (canvas.height = window.innerHeight);
-// const ctx = canvas.getContext("2d");
-
-// ctx.fillStyle = "rgb(255 0 0)";
-// ctx.fillRect(50, 50, 100, 150);
-
-// ctx.fillStyle = "rgb(0 255 0 / 75%)";
-// ctx.fillRect(75, 75, 100, 100);
